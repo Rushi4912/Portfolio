49a327a0154d8f0aa81bde8aaf180944de4cfe08
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-11 23:51:09 +0530

    add dark mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css';
 import { Header } from '@/components/Header';
 import Footer from '@/components/Footer';
+import { ThemeProvider } from '@/components/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +13,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light">
         <Header/>
         {children}
-        <Footer/>
+          <Footer />
+          
+          </ThemeProvider>
       </body>
     </html>
   )
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import ThemeTogler from './ThemeTogler';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+       <div>
+            <ThemeTogler/>
+    </div>
   )
 }
