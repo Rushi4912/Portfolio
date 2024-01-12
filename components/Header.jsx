0b5ef5b6e88e96bbe13bb2e4d99ab14db87a545a
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-12 23:42:27 +0530

    change

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import Logo from './ui/Logo'
 import { useState,useEffect } from 'react';
 import ThemeToggler from './ThemeToggler';
 import Nav from './ui/Nav';
+import MobileNav from './ui/MobileNav';
 
 export const Header = () => {
   return (
@@ -12,9 +13,13 @@ export const Header = () => {
       <div className="container mx-auto">
         <div className='flex justify-between items-center'>
           <Logo />
-          <div>
-             <Nav/>
-           <ThemeToggler/>
+          <div className='flex items-center gap-x-6'>
+             <Nav containerStyles='hidden xl:flex gap-x-8 item-center'/>
+            <ThemeToggler />
+            
+            <div className='xl:hidden'>
+              <MobileNav/>
+            </div>
 
           </div>
         
--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+const links = [
 
-const Nav = () => {
+     { path: './', name: 'home' },
+     { path: './projects', name: 'my projects' },
+     { path: './contact', name: 'contact' }
+];
+
+const Nav = ({containerStyles,linkStyles,underlineStyles}) => {
   return (
-    <div>Nav</div>
+       <nav className={`${containerStyles}`}>
+            {links.map((link, index) => {
+                 return <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>{link.name}</Link>
+            })}
+    </nav>
   )
 }
 
