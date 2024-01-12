35ac72e66d938d6702496b35ab6751c0b468e46d
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-12 23:51:27 +0530

    change

--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 const links = [
 
      { path: './', name: 'home' },
@@ -10,7 +11,20 @@ const Nav = ({containerStyles,linkStyles,underlineStyles}) => {
   return (
        <nav className={`${containerStyles}`}>
             {links.map((link, index) => {
-                 return <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>{link.name}</Link>
+                 return <Link href={link.path 
+                 } key={index} className={`capitalize ${linkStyles}`}>
+                      {link.path === path && (
+                           
+                           <motion.span
+                           
+                                initial={{ y: '-100%' }}
+                                animate={{ y: 0 }}
+                                transition={{ type: 'tween' }}
+                                layoutId='underline'
+                                className={`${underlineStyles}`}
+                           />
+                      )}
+                      {link.name}</Link>
             })}
     </nav>
   )
