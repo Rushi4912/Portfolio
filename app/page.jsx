984908268bd5a4b3d39f7c0c0473b26c46e3ef91
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-13 23:04:07 +0530

    hero

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
+import Hero from '@/components/Hero';
 
 export default function Home() {
-  return <main>HomePage</main>
+  return <main>
+
+    <Hero/>
+  </main>
 }
