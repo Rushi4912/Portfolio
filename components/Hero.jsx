042d1939a0cec81a442e2347eeb37c8e81b5284a
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-14 21:35:14 +0530

    change

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
                       </div>
                       <div className='hidden xl:flex relative'>
                            
-                           <div className='bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-23'></div>
+                           <div className='bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2'></div>
                       </div>
                  </div>
 
