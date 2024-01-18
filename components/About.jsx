f5d4023189b3d691d09734012bb3543a6d0592a7
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-18 23:53:20 +0530

    change

--- components/About.jsx
+++ components/About.jsx
@@ -110,11 +110,44 @@ const skillData = [
 
 
 const About = () => {
-  return (
-       <div>
-            
+     const getData = (arr, title) =>{
+          return arr.find((item) => item.title === title);
+     };
 
-    </div>
+
+  return (
+       <section className='xl:h-[860px] pb-12 xl:py-24'>
+            
+            <div className='container mx-auto'>
+                 
+                 <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>About me</h2>
+
+                 <div className='flex flex-col xl:flex-row'>
+                      
+                      <div className='hidden xl:flex flex-1 relative'>
+                           
+                           <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/developer.png'/>
+                      </div>
+                      <div className='flex-1'>
+                           
+                           <Tabs defaultValue='personal'>
+                                
+                                <TabList className ='w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none'>
+                                     <TabTrigger value ='personal'> Personal Info</TabTrigger>
+                                     <TabTrigger value ='qualifications'> Qualification</TabTrigger>
+                                        <TabTrigger value='skills'> Skills</TabTrigger>
+                                </TabList>
+                                <div className='text-lg mt-12 xl:mt-8'>
+                                     <TabContent value='personal'> personal info</TabContent>
+                                     <TabContent value='qualifications'> qualifications info</TabContent>
+                                      <TabContent value='skills'> skills info</TabContent>
+                                </div>
+                             </Tabs>
+                      </div>
+                 </div>
+            </div>
+
+    </section>
   )
 }
 
