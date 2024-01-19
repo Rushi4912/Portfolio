87bfc54908e0b0629a42676d9117ec3dc8112710
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-19 22:30:25 +0530

    change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
-import { Inter } from 'next/font/google'
+import { Outer } from 'next/font/google'
 import './globals.css';
 import { Header } from '@/components/Header';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const Outer = Outer({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={Outer.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
         <Header/>
         {children}
--- components/About.jsx
+++ components/About.jsx
@@ -54,13 +54,13 @@ const qualificationData = [
                
               
                 {
-                    university: 'Freelancer',
-                    qualification: 'Full Stack Engineer',
+                    company: 'Freelancer',
+                    role: 'Full Stack Engineer',
                     years :'2023-2025',
                },
                 {
                     company: 'Open AI',
-                    qualification: 'AI Engineer',
+                    role: 'AI Engineer',
                     years :'2030-2032'
                }
      
@@ -140,13 +140,81 @@ const About = () => {
                                 <div className='text-lg mt-12 xl:mt-8'>
                                      <TabsContent value='personal'>
                                      
-                                          <div>
+                                          <div className='text-center xl:text-left'>
                                                <h3 className='h3 mb-4'>Unmatched Service Quality for Over 2 Years</h3>
                                                <p className='subtitle max-w-xl max-auto xl:max-0'> I specialize in crafting intuitive websites with cutting-edge technology ,delivering dynamic and engaging user experiences.</p>
+                                         // icons
+                                               <div className='grid xl:grid-cols-2 gap-4 mb-12'>
+                                                    {
+                                                         InfoData.map((item, index) => {
+                                                              
+                                                              return (
+                                                                   <div className='flex items-center gap-x-4 mx-auto xl:mx-0' key={index}>
+                                                                        <div className='text-primary'>{item.icon}</div>
+                                                                        <div>{ item.text}</div>
+
+                                                                   </div>
+                                                              );
+                                                         })
+                                                    }
+
+                                               </div>
+
+                                               // languages
+                                               <div className='flex flex-col gap-y-2'>
+                                                    
+                                                    <div className='text-primary'>Language Skill</div>
+                                                    <div className="border-b border-border"></div>
+                                                    <div>English,Japanese ,Hindi ,Marathi</div>
+                                               </div>
                                      </div>
                                      
                                      </TabsContent>
-                                     <TabsContent value='qualifications'> qualifications info</TabsContent>
+                                     <TabsContent value='qualifications'>
+                                          <div>
+                                               
+                                               <h3 className='h3 mb-8 text-center xl:text-left'>My Awesome Journey</h3>
+                                               {/* experience and education wrapper */}
+                                               <div>
+                                                 {/* experience     */}
+                                                    <div>
+                                                         
+                                                         <div className='flex gap-x-4 items-center text-[22px]'>
+                                                              <BriefCase/>   
+
+                                                              <h4 className='capitalize font-medium'>
+                                                                   {
+                                                                        getData(qualificationData,'experience').title
+                                                                   }
+                                                              </h4>
+                                                         </div>
+                                                         {/* list  */}
+
+                                                         <div>
+                                                              
+                                                              {
+                                                                   getData(qualificationData, 'experience').data.map((item, index) => {
+                                                                        const { company, role, years } = item;
+                                                                        return (
+                                                                             <div>
+                                                                                  
+                                                                                  <div></div>
+                                                                                  <div></div>
+                                                                                  <div></div>
+                                                                             </div>
+                                                                        );
+                                                                   })
+                                                              }
+                                                         </div>
+                                                    </div>
+                                                    
+                                                    {/* education */}
+                                               <div>education</div>
+
+                                               </div>
+                                     </div>
+                                     
+                                     </TabsContent>
                                       <TabsContent value='skills'> skills info</TabsContent>
                                 </div>
                              </Tabs>
