876b4b861afd905ae22b4a3d7bc5599db6929d18
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-19 23:11:38 +0530

    change

--- components/About.jsx
+++ components/About.jsx
@@ -262,7 +262,52 @@ const About = () => {
                     </div>
                   </div>
                 </TabsContent>
-                <TabsContent value="skills"> skills info</TabsContent>
+                <TabsContent value="skills">
+                
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">What I Use EveryDay</h3>
+                    <div className="mb-16">
+                      <h4 className="text-4xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* skills list  */}
+                      <div>
+
+                        {
+
+                          getData(skillData, 'skills').data.map((item, index) => {
+                            
+                            const { name } = item;
+                            return (
+                              <div className="w-2/4 text-center xl:text-left mx-auto xl:mx-0" key={index}>
+
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            );
+
+                          })
+                        }
+                      </div>
+                    </div>
+                    {/* tools  */}
+                    <div>
+
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">Tools</h4>
+                      <div className="border-b border-border mb-8"></div>
+                         {/* tool list  */}
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, 'tools').data.map((item, index) => {
+                           
+                          const { imgPath } = item;
+                          return <div key={index}>
+
+                            <Image src={imgPath} width={48} height={48} alt='' priority/>
+                          </div>
+                         })}
+                      </div>
+                    </div>
+                </div>
+                
+                </TabsContent>
               </div>
             </Tabs>
           </div>
