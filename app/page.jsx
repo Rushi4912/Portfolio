b3446a5a2d1222223cdecd21d2332780c12558f5
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-21 22:53:24 +0530

    add project section

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,15 @@ import Image from 'next/image';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Services from '@/components/Services';
+import Work from '@/components/Work';
+
 
 export default function Home() {
   return <main>
 
     <Hero />
     <About />
-    <Services/>
+    <Services />
+    <Work/>
   </main>
 }
