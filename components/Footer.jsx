fb34b0955b250638696fca542e36e6a9222afe1a
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-23 23:16:57 +0530

    add cta

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Socials from './Socials';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-secondary py-12'>
+
+      <div className='container mx-auto'>
+
+        <div className='flex flex-col items-center justify-between '>
+          {/* socials */}
+          <Socials  containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' iconsStyles='text-primary text-[20px] dark:text-white/70 hover:text-white dark:hover:text-primary transition-all '/>
+          {/* copyright      */}
+          <div className='text-muted-foreground'>Copyright &copy;Rushikesh Pawar. All right reserved</div>
+        </div>
+      </div>
+
+    </footer>
   )
 }
 
