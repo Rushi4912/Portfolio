c7111f0a4b4cf2ca576f15ea345dd278e1bd0778
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-27 00:29:58 +0530

    delete vercel analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import Image from 'next/image';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
@@ -7,36 +7,36 @@ import Work from '@/components/Work';
 import Reviews from '@/components/Reviews';
 import Cta from '@/components/Cta';
 // import { useRouter } from "next/router";
-import { useRouter } from 'next/navigation'; // Updated import statement
-import analytics from "@vercel/analytics";
-import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation'; // Updated import statement
+// import analytics from "@vercel/analytics";
+// import { useEffect } from 'react';
 
 
 export default function Home() {
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      // Track pageview when the route changes
-      analytics.page();
-    };
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     // Track pageview when the route changes
+  //     analytics.page();
+  //   };
 
-    // Initialize analytics
-    analytics.init({
-      // Add your project's deployment URL here
-      app: "portfolio-94cv2p7nz-rushikesh-pawars-projects.vercel.app",
-    });
+  //   // Initialize analytics
+  //   analytics.init({
+  //     // Add your project's deployment URL here
+  //     app: "portfolio-94cv2p7nz-rushikesh-pawars-projects.vercel.app",
+  //   });
 
-    // Track the initial pageview
-    analytics.page();
+  //   // Track the initial pageview
+  //   analytics.page();
 
-    // Set up a listener for route changes
-    router.events.on("routeChangeComplete", handleRouteChange);
+  //   // Set up a listener for route changes
+  //   router.events.on("routeChangeComplete", handleRouteChange);
 
-    // Clean up the listener when the component unmounts
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, []);
+  //   // Clean up the listener when the component unmounts
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, []);
   return <main>
 
     <Hero />
