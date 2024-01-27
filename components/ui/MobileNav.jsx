4f780796bfe0f657e805f7d242a763cb8b348687
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-01-27 23:55:04 +0530

    fix

--- components/ui/MobileNav.jsx
+++ components/ui/MobileNav.jsx
@@ -4,34 +4,34 @@ import { AlignJustify } from 'lucide-react';
 import Nav from './Nav';
 import Logo from './Logo';
 import Socials from '../Socials';
-import React, { useState } from "react";
-import { useRouter } from 'next/router';  // Import useRouter from Next.js
+// import React, { useState,useEffect } from "react";
+// import { useRouter } from 'next/router';  // Import useRouter from Next.js
 
 const MobileNav = () => {
-  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
-  const router = useRouter(); // Initialize useRouter
+  // const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+  // const router = useRouter(); // Initialize useRouter
 
-  const closeMobileNav = () => {
-    setMobileNavOpen(false);
-  };
+  // const closeMobileNav = () => {
+  //   setMobileNavOpen(false);
+  // };
 
-  const handleSheetTriggerClick = () => {
-    setMobileNavOpen(!isMobileNavOpen);
-  };
+  // const handleSheetTriggerClick = () => {
+  //   setMobileNavOpen(!isMobileNavOpen);
+  // };
 
-  // Close the mobile nav when a navigation link is clicked
-  const handleNavLinkClick = () => {
-    closeMobileNav();
-  };
+  // // Close the mobile nav when a navigation link is clicked
+  // const handleNavLinkClick = () => {
+  //   closeMobileNav();
+  // };
 
-  // Use useEffect to close the mobile nav when navigating to another page
-  useEffect(() => {
-    closeMobileNav();
-  }, [router.pathname]); // Trigger the effect when the pathname changes
+  // // Use useEffect to close the mobile nav when navigating to another page
+  // useEffect(() => {
+  //   closeMobileNav();
+  // }, [router.pathname]); // Trigger the effect when the pathname changes
 
   return (
     <Sheet>
-      <SheetTrigger asChild>
+      <SheetTrigger asChild >
         <AlignJustify className="cursor-pointer" />
       </SheetTrigger>
       <SheetContent>
@@ -41,6 +41,7 @@ const MobileNav = () => {
             <Nav
               containerStyles="flex flex-col items-center gap-y-6"
               linkStyles="text-2xl"
+              
             />
           </div>
           <Socials containerStyles="flex gap-x-4 " iconsStyles="text-2xl" />
--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
@@ -10,27 +11,33 @@ const links = [
 ];
 
 const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
-     const path = usePathname();
+  const path = usePathname();
   return (
-       <nav className={`${containerStyles}`}>
-            {links.map((link, index) => {
-                 return <Link href={link.path 
-                 } key={index} className={`capitalize ${linkStyles}`}>
-                      {link.path === path && (
-                           
-                           <motion.span
-                           
-                                initial={{ y: '-100%' }}
-                                animate={{ y: 0 }}
-                                transition={{ type: 'tween' }}
-                                layoutId='underline'
-                                className={`${underlineStyles}`}
-                           />
-                      )}
-                      {link.name}</Link>
-            })}
+    <nav className={`${containerStyles}`}>
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`capitalize ${linkStyles}`}
+          >
+            {link.path === path && (
+              <motion.span
+                initial={{ y: "-100%" }}
+                animate={{ y: 0 }}
+                transition={{ type: "tween" }}
+                layoutId="underline"
+                className={`${underlineStyles}`}
+              />
+            )}
+            {link.name}
+          </Link>
+        );
+      })}
     </nav>
-  )
-}
+  );
+};
 
 export default Nav;
+
+
